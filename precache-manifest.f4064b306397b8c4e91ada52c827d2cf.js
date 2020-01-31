11153db02c372032649ad1b570e125f2442edeaf
Mackenzie Ros <mackenzieros19@gmail.com>
2020-01-31 15:07:47 -0800

    Updates

---
+++ precache-manifest.f4064b306397b8c4e91ada52c827d2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf56c45c4d40e1ceeacaf7a5e8c40ec",
+    "url": "/mackenzieros.github.io/index.html"
+  },
+  {
+    "revision": "cb1b2f25e6937941dbdb",
+    "url": "/mackenzieros.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ba11ac28cc58b0eac35",
+    "url": "/mackenzieros.github.io/static/js/2.07ac40ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mackenzieros.github.io/static/js/2.07ac40ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb1b2f25e6937941dbdb",
+    "url": "/mackenzieros.github.io/static/js/main.d63a0a3d.chunk.js"
+  },
+  {
+    "revision": "b19f0bf186a5abdbd9f6",
+    "url": "/mackenzieros.github.io/static/js/runtime-main.a7d58ef5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.e14299308e6d9d9010e7df0386c3c90b.js"
+  "/mackenzieros.github.io/precache-manifest.f4064b306397b8c4e91ada52c827d2cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mackenzieros.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
