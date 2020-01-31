90b87f34c06d6d5c1b66931bf274bdcdc7c7b8d3
Mackenzie Ros <mackenzieros19@gmail.com>
2020-01-31 15:25:58 -0800

    Updates

---
+++ precache-manifest.a1b31f8cc9b6653b4baa5093dcc0842d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0dc242651efc4dc0259d5c0cd57d8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7195ea1b642bec5e8b8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ba11ac28cc58b0eac35",
+    "url": "/static/js/2.07ac40ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.07ac40ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7195ea1b642bec5e8b8",
+    "url": "/static/js/main.fe088045.chunk.js"
+  },
+  {
+    "revision": "37136e5acd31e79fc52a",
+    "url": "/static/js/runtime-main.0d6658c7.js"
+  }
+]);
