4db07bce1f1ee4b93fa88a62a424bc37ab0d283a
Mackenzie Ros <mackenzieros19@gmail.com>
2020-01-31 15:34:57 -0800

    Updates

---
+++ precache-manifest.42d1bd74387f0b7f94d57c4bd71922ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405a48de76c8163e0614730554741510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7195ea1b642bec5e8b8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ba11ac28cc58b0eac35",
+    "url": "/static/js/2.07ac40ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.07ac40ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7195ea1b642bec5e8b8",
+    "url": "/static/js/main.fe088045.chunk.js"
+  },
+  {
+    "revision": "37136e5acd31e79fc52a",
+    "url": "/static/js/runtime-main.0d6658c7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mackenzieros.github.io/precache-manifest.f4064b306397b8c4e91ada52c827d2cf.js"
+  "/precache-manifest.42d1bd74387f0b7f94d57c4bd71922ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mackenzieros.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
